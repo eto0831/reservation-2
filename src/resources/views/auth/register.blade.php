b6a983250cf393d473004ca4985e43c1260b09e7
eto0831 <eto0831@gmail.com>
2024-12-13 17:07:44 +0900

    register view added

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -6,59 +6,52 @@
 
 @section('content')
 <div class="register__content">
-    <div class="register-form__heading">
-        <h2>会員登録</h2>
+    <div class="register__wrapper">
+        <div class="register-form__heading">
+            <h3>Register</h3>
+        </div>
+        <form class="form" action="/register" method="post">
+            @csrf
+            <div class="form__group">
+                <div class="form__group-content">
+                    <div class="form__input--text name">
+                        <input type="text" name="name" value="{{ old('name') }}" placeholder="Name" />
+                    </div>
+                    <div class="form__error">
+                        @error('name')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__group-content">
+                    <div class="form__input--text mail">
+                        <input type="email" name="email" value="{{ old('email') }}" placeholder="Email"/>
+                    </div>
+                    <div class="form__error">
+                        @error('email')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__group-content">
+                    <div class="form__input--text password">
+                        <input type="password" name="password" placeholder="Password"/>
+                    </div>
+                    <div class="form__error">
+                        @error('password')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="form__button">
+                <button class="form__button-submit blue-button" type="submit">登録</button>
+            </div>
+        </form>
     </div>
-    <form class="form" action="/register" method="post">
-        @csrf
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">お名前</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="text" name="name" value="{{ old('name') }}" />
-                </div>
-                <div class="form__error">
-                    @error('name')
-                    {{ $message }}
-                    @enderror
-                </div>
-            </div>
-        </div>
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">メールアドレス</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="email" name="email" value="{{ old('email') }}" />
-                </div>
-                <div class="form__error">
-                    @error('email')
-                    {{ $message }}
-                    @enderror
-                </div>
-            </div>
-        </div>
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">パスワード</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="password" name="password" />
-                </div>
-                <div class="form__error">
-                    @error('password')
-                    {{ $message }}
-                    @enderror
-                </div>
-            </div>
-        </div>
-        <div class="form__button">
-            <button class="form__button-submit blue-button" type="submit">登録</button>
-        </div>
-    </form>
 </div>
 @endsection
