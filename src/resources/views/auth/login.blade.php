a4efe2ca0474ad2da85718cc6438ed46d262d5fe
eto0831 <eto0831@gmail.com>
2024-12-13 13:56:04 +0900

    login css change

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -6,44 +6,46 @@
 
 @section('content')
 <div class="login__content">
-    <div class="login-form__heading">
-        <h2>ログイン</h2>
+    <div class="login__wrapper">
+        <div class="login-form__heading">
+            <h3>Login</h3>
+        </div>
+        <form class="form" action="/login" method="post">
+            @csrf
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">メールアドレス</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="email" name="email" value="{{ old('email') }}" />
+                    </div>
+                    <div class="form__error">
+                        @error('email')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="form__group">
+                <div class="form__group-title">
+                    <span class="form__label--item">パスワード</span>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="password" name="password" />
+                    </div>
+                    <div class="form__error">
+                        @error('password')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            <div class="form__button">
+                <button class="form__button-submit blue-button" type="submit">ログイン</button>
+            </div>
+        </form>
     </div>
-    <form class="form" action="/login" method="post">
-        @csrf
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">メールアドレス</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="email" name="email" value="{{ old('email') }}" />
-                </div>
-                <div class="form__error">
-                    @error('email')
-                    {{ $message }}
-                    @enderror
-                </div>
-            </div>
-        </div>
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">パスワード</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="password" name="password" />
-                </div>
-                <div class="form__error">
-                    @error('password')
-                    {{ $message }}
-                    @enderror
-                </div>
-            </div>
-        </div>
-        <div class="form__button">
-            <button class="form__button-submit blue-button" type="submit">ログイン</button>
-        </div>
-    </form>
 </div>
 @endsection
