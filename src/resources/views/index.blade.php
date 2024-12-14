399152e147aae2b9a018f7630c622828dc161664
eto0831 <eto0831@gmail.com>
2024-12-14 14:17:23 +0900

    index css change

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -11,36 +11,39 @@
 </div>
 
 <div class="attendance__content">
-    <form class="search-form" action="/search" method="get">
-        @csrf
-        <div class="contact-search">
-            <select class="search-form__item-select" name="area_id">
-                <option value="">All area</option>
-                @foreach ($areas as $area)
-                <option value="{{ $area->id }}" @if(request('area_id') == $area->id) selected @endif>
-                    {{ $area->area_name }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="contact-search">
-            <select class="search-form__item-select" name="genre_id">
-                <option value="">All genre</option>
-                @foreach ($genres as $genre)
-                <option value="{{ $genre->id }}" @if(request('genre_id') == $genre->id) selected @endif>{{ $genre->genre_name }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="name-search">
-            <input type="text" class="search-form__item-input" placeholder="Search..." name="keyword"
-                value="{{ request('keyword') ?? old('keyword') }}">
-        </div>
-        {{-- <div class="search-form__button">
-            <button class="search-form__button-submit" type="submit">検索</button>
-        </div> --}}
-    </form>
+    <div class="search-form__wrapper">
+        <form class="search-form" action="/search" method="get">
+            @csrf
+            <div class="contact-search">
+                <select class="search-form__item-select area-select" name="area_id">
+                    <option value="">All area</option>
+                    @foreach ($areas as $area)
+                    <option value="{{ $area->id }}" @if(request('area_id')==$area->id) selected @endif>
+                        {{ $area->area_name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="contact-search">
+                <select class="search-form__item-select genre-select" name="genre_id">
+                    <option value="">All genre</option>
+                    @foreach ($genres as $genre)
+                    <option value="{{ $genre->id }}" @if(request('genre_id')==$genre->id) selected @endif>{{
+                        $genre->genre_name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="name-search">
+                <input type="text" class="search-form__item-input" placeholder="Search..." name="keyword"
+                    value="{{ request('keyword') ?? old('keyword') }}">
+            </div>
+            {{-- <div class="search-form__button">
+                <button class="search-form__button-submit" type="submit">検索</button>
+            </div> --}}
+        </form>
+    </div>
     <div class="shop__wrap">
         @foreach ($shops as $shop)
-            @include('components.shop-card', ['shop' => $shop])
+        @include('components.shop-card', ['shop' => $shop])
         @endforeach
     </div>
     {{ $shops->withQueryString()->links('vendor.pagination.custom') }}
