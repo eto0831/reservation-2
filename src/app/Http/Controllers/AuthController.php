24e06c28ae12688f1f221ab31274b03f52aedcb9
eto0831 <eto0831@gmail.com>
2025-01-27 23:39:11 +0900

    mypage fixed

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -19,7 +19,12 @@ class AuthController extends Controller
         $reservations = auth()->user()->reservations()->with('shop')->get();
         $areas = Area::all();
         $genres = Genre::all();
-        $favorites = auth()->user()->favorites()->with(['shop.area', 'shop.genre'])->get();
-        return view('mypage.index', compact('reservations', 'areas', 'genres','favorites'));
+        $favorites = auth()->user()->favorites()
+            ->with(['shop.area', 'shop.genre']) // 関連データを取得
+            ->with(['shop' => function ($query) {
+                $query->withCount('reviews'); // reviews_countを追加
+            }])->get();
+
+        return view('mypage.index', compact('reservations', 'areas', 'genres', 'favorites'));
     }
 }
